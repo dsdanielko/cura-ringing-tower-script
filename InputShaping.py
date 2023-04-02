ae8838495d959dd092762de8d9b9ebe3c41b4644
Daniel Ko <dsk13@ic.ac.uk>
2023-04-02 01:23:37 +0100

    Add files via upload

---
+++ InputShaping.py
@@ -0,0 +1,56 @@
+import json
+import re
+
+from ..Script import Script
+
+
+class InputShaping(Script):
+    def __init__(self):
+        super().__init__()
+
+    def getSettingDataString(self):
+        return json.dumps({
+            'name': 'Input Shaping',
+            'key': 'Input Shaping',
+            'metadata': {},
+            'version': 2,
+            'settings': {
+                'start_f': {
+                    'label': 'Start frequency',
+                    'description': 'Frequency sweep start value',
+                    'unit': 'Hz',
+                    'type': 'int',
+                    'default_value': 15
+                },
+                'end_f': {
+                    'label': 'End frequency',
+                    'description': 'Frequency sweep end value',
+                    'unit': 'Hz',
+                    'type': 'int',
+                    'default_value': 60
+                },
+                'start_l': {
+                    'label': 'Start layer',
+                    'description': 'Layer to start modifying frequency from',
+                    'unit': '',
+                    'type': 'int',
+                    'default_value': 2
+                }
+            }
+        })
+
+    def execute(self, data):
+        start_hz = self.getSettingValueByKey('start_f')
+        end_hz = self.getSettingValueByKey('end_f')
+        start_layer = self.getSettingValueByKey('start_l')
+
+        for i, layer in enumerate(data):
+            lines = layer.split('\n')
+            for j, line in enumerate(lines):
+                if line.startswith(';LAYER:'):
+                    layer = float(line.strip(';LAYER:'))
+                    hz = 0 if layer < start_layer else start_hz + end_hz * (layer - start_layer) / 297
+                    lines[j] += '\n;TYPE:INPUTSHAPING\nM593 F%f' % hz
+            data[i] = '\n'.join(lines)
+
+        return data
